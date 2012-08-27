dbf88b30c16114328d01f90a6026c85f806463d3
Val Schuman <github@schuman.ws>
2012-08-27 11:24:41 -0400

    Added description comments

--- American_Safety_Classes.js
+++ American_Safety_Classes.js
@@ -1,3 +1,9 @@
+// Code to help speed along the taking of the 6 hour Driver Safety course at American Safety Classes
+// The code checks for the timer that prevents the user from advancing to the next screen 
+// until the timer expires, and when the timer expires automatically clicks on the "Next" button.
+// The user still needs to monitor the progress of the course to answer the questions that pop up
+// from time to time to verify the user identity.
+
 // Add jQuery to the page
 var jqueryscript = document.createElement("script"); jqueryscript.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"; document.body.appendChild(jqueryscript);
 
