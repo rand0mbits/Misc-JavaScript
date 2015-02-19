e068e372f8cc6aa411eb1c093cd10fc8cf5a67d4
Val Schuman <github@schuman.ws>
2015-02-19 17:20:24 -0500

    Added Array.prototype.findIntegerGap

--- collection.utils.js
+++ collection.utils.js
@@ -9,3 +9,29 @@ if (typeof Array.prototype.toMap != 'function') {
 	    return out;
 	};
 }
+
+/**
+ * Given an array of numbers, such as [5,3,6,1], sorts it, 
+ * and finds the first available integer greater than or equal to "start" (which defaults to 1);
+ * WARNING: passed in array will be sorted in ascending order.
+ * @author Val Schuman
+ */
+if (typeof Array.prototype.findIntegerGap != 'function') {
+	Array.prototype.findIntegerGap = function(start) {
+		if (!start && start !== 0) {
+			start = 1;
+		}
+		this.sort();
+		if (this[0] != start) {
+			return start;
+		}
+		for (var i = 0; i < this.length; i++) {
+			if (i + 1 == length) {
+				return this[i] + 1;
+			}
+			if ((this[i] + 1) < this[i + 1]) {
+				return this[i] + 1;
+			}
+		}
+	};
+}
