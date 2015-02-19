33226ebe874db175695136cbe49b894e7bf5c05f
Val Schuman <github@schuman.ws>
2015-02-19 17:48:26 -0500

    Updated findIntegerGap

--- collection.utils.js
+++ collection.utils.js
@@ -30,15 +30,17 @@ if (typeof Array.prototype.findIntegerGap != 'function') {
 			start = 1;
 		}
 		this.sort();
-		if (this[0] != start) {
+		if (this.length === 0 || start < this[0] || start > this[this.length - 1]) {
 			return start;
 		}
 		for (var i = 0; i < this.length; i++) {
+			var nextNum = this[i] + 1;
 			if (i + 1 == length) {
-				return this[i] + 1;
+				return nextNum;
 			}
-			if ((this[i] + 1) < this[i + 1]) {
-				return this[i] + 1;
+			
+			if (start < nextNum && nextNum < this[i + 1]) {
+				return nextNum;
 			}
 		}
 	};
