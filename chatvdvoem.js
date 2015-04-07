f9766c1c8fc3ac29a68c2ae12752e699d16f9893
Val Schuman <github@schuman.ws>
2015-04-07 12:55:59 -0400

    cleanup

--- chatvdvoem.js
+++ chatvdvoem.js
@@ -1,13 +1,13 @@
-var header = $('#header')
+var header = $('#header');
 header.find('h2').remove();
 var custom = $('<div>');
 header.find('h1').after(custom);
-var restart = $('<button>')
+var restart = $('<button>');
 restart.text('Restart');
 restart.click(function() {
-		chat_stop();
-		setTimeout(function() {
-			$('#chat_start').click();
-		}, 500);
+	chat_stop();
+	setTimeout(function() {
+		$('#chat_start').click();
+	}, 500);
 });
-custom.append(restart)
+custom.append(restart);
