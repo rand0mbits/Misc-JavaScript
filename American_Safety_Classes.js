becc676ed84b3436540ecc2e894c83c392e5a6c6
Val Schuman <github@schuman.ws>
2015-04-07 11:35:14 -0400

    Comment fix + chatvdvoem

--- American_Safety_Classes.js
+++ American_Safety_Classes.js
@@ -13,6 +13,6 @@ var jqueryscript = document.createElement("script"); jqueryscript.src = "https:/
 // click the "Next" button
 // $(window.frames[1].document).find("#next_top")[0].click()
 
-// Create an interval to execute every N seconds, check if the timer exists, if it doesn't, log that it doesn't and do nothhing
+// Create an interval to execute every N seconds, check if the timer exists, if it doesn't, log that it doesn't and do nothing
 // if it does, check if it's at "00:00:00", if it is, click the "Next" button.
 var myinterval = setInterval( function() { if ( $(window.frames[1].document).find("#TimeRemainingClock").length == 0 ) { console.log(new Date() + ": no clock. skipping."); } else if ($(window.frames[1].document).find("#TimeRemainingClock").text() == "00:00:00") { console.log(new Date() + ": clicking next" ); $(window.frames[1].document).find("#next_top")[0].click(); } }, 3000 );
---
+++ chatvdvoem.js
@@ -0,0 +1,13 @@
+var header = $('#header')
+header.find('h2').remove();
+var custom = $('<div>');
+header.find('h1').after(custom);
+var restart = $('<button>')
+restart.text('Restart');
+restart.click(function() {
+		chat_stop();
+		setTimeout(function() {
+			$('#chat_start').click();
+		}, 200);
+});
+custom.append(restart)
